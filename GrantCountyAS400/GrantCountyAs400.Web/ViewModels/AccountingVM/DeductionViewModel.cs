15951e3f47d4c656524a98568a72fe03642c443e
Ebram Khalil <ebram_tharwat@hotmail.com>
2018-07-14 21:50:29 +0200

    Display deduction code.

--- GrantCountyAS400/GrantCountyAs400.Web/ViewModels/AccountingVM/DeductionViewModel.cs
+++ GrantCountyAS400/GrantCountyAs400.Web/ViewModels/AccountingVM/DeductionViewModel.cs
@@ -5,12 +5,16 @@ namespace GrantCountyAs400.Web.ViewModels.AccountingVM
     public class DeductionViewModel
     {
         public int Id { get; set; }
+
+        [Display(Name = "Year")]
         public int MiscYear { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:00000}")]
         public decimal EmployeeNumber { get; set; }
 
+        [Display(Name = "Code")]
         public string MiscCode { get; set; }
+
         public decimal MiscDeduction { get; set; }
         public decimal MiscBenefit { get; set; }
     }
