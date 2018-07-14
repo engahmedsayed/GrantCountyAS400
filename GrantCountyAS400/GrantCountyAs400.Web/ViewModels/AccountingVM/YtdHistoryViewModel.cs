9e58691b0a728c107343b4f343b20e8782f7a34d
Ebram Khalil <ebram_tharwat@hotmail.com>
2018-07-14 21:51:15 +0200

    Update column display names of ytd history

--- GrantCountyAS400/GrantCountyAs400.Web/ViewModels/AccountingVM/YtdHistoryViewModel.cs
+++ GrantCountyAS400/GrantCountyAs400.Web/ViewModels/AccountingVM/YtdHistoryViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace GrantCountyAs400.Web.ViewModels.AccountingVM
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrantCountyAs400.Web.ViewModels.AccountingVM
 {
     public class YtdHistoryViewModel
     {
@@ -9,13 +11,28 @@
         public decimal Number { get; set; }
         public decimal Fund { get; set; }
         public decimal Department { get; set; }
+
+        [Display(Name = "Gross Pay")]
         public decimal YearlyGrossPay { get; set; }
+
+        [Display(Name = "Gross Pay Deferred")]
         public decimal YearlyGrossPayDeferred { get; set; }
+
+        [Display(Name = "Non Taxable")]
         public decimal YearlyNonTaxable { get; set; }
+
+        [Display(Name = "Taxable")]
         public decimal YearlyTaxable { get; set; }
+
+        [Display(Name = "Fica Gross")]
         public decimal YearlyFicaGross { get; set; }
+
+        [Display(Name = "Federal With Holding")]
         public decimal YearlyFederalWithholding { get; set; }
+
+        [Display(Name = "Stat With Holding")]
         public decimal YearlyStatWithholding { get; set; }
+
         public decimal YearlyFica { get; set; }
         public decimal YearlyFicaBenefit { get; set; }
         public decimal YearlyMedicalGross { get; set; }
