445c47a6b05dc56fdf4180205b9a2d8494d4ead3
Ebram Khalil <ebram_tharwat@hotmail.com>
2018-07-18 20:54:05 +0200

    Update headings for deduction tab

--- GrantCountyAS400/GrantCountyAs400.Web/ViewModels/AccountingVM/DeductionViewModel.cs
+++ GrantCountyAS400/GrantCountyAs400.Web/ViewModels/AccountingVM/DeductionViewModel.cs
@@ -10,12 +10,16 @@ namespace GrantCountyAs400.Web.ViewModels.AccountingVM
         public int MiscYear { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:00000}")]
+        [Display(Name = " Employee #")]
         public decimal EmployeeNumber { get; set; }
 
         [Display(Name = "Code")]
         public string MiscCode { get; set; }
 
+        [Display(Name = "Deduction")]
         public decimal MiscDeduction { get; set; }
+
+        [Display(Name = "Benefit")]
         public decimal MiscBenefit { get; set; }
     }
 }
