53e8e5a9c1bcf6eeb81d90dfb42eb6e0be279a03
Ahmed Sayed Mostafa <ahmed.sayed@itworx.com>
2018-09-07 19:10:16 +0300

    Make PermitNumber Permit Number Make PermidAdeenDate Seq Make PermitIssueDate Issue Date Make JurisdictionCode Jurisdiction Make PercentComplete % Complete Make EstimatedValue Estimated Value Make NewOrDemo N/D

--- GrantCountyAS400/GrantCountyAs400.Web/ViewModels/Assessment/BuildingPermits/BuildingPermitsViewModel.cs
+++ GrantCountyAS400/GrantCountyAs400.Web/ViewModels/Assessment/BuildingPermits/BuildingPermitsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,18 @@ namespace GrantCountyAs400.Web.ViewModels.Assessment.BuildingPermits
 {
     public class BuildingPermitsViewModel
     {
+        [Display(Name ="Parcel Number")]
         public decimal? ParcelNumber { get; set; }
 
+        [Display(Name = "Tax Payer")]
         public string TaxPayer { get; set; }
 
         public string Owner { get; set; }
 
+        [Display(Name ="Description")]
         public string FirstDescription { get; set; }
 
+        [Display(Name = "Land Use")]
         public string LandUse { get; set; }
 
         public string SecondDescription { get; set; }
@@ -35,18 +40,25 @@ namespace GrantCountyAs400.Web.ViewModels.Assessment.BuildingPermits
 
     public class BuildingPermitsLineValueViewModel
     {
+        [Display(Name ="Permit Number")]
         public decimal? PermitNumber { get; set; }
 
+        [Display(Name = "Seq")]
         public decimal? PermitAddenDum { get; set; }
 
+        [Display(Name = "Issue Date")]
         public DateTime? PermitIssueDate { get; set; }
 
+        [Display(Name = "Jurisdiction")]
         public string JurisdictionCode { get; set; }
 
+        [Display(Name = "% Complete")]
         public decimal? PercentComplete { get; set; }
 
+        [Display(Name = "Estimated Value")]
         public decimal? EstimatedValue { get; set; }
 
+        [Display(Name = "N/D")]
         public string NewOrDemo { get; set; }
 
         public string Comment { get; set; }
