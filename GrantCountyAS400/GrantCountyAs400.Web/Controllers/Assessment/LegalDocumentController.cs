98e008f72284e7132e6e20db335d3490b6f81c2f
Ebram Khalil <ebram_tharwat@hotmail.com>
2018-09-16 18:07:52 +0200

    Add legal document pages

---
+++ GrantCountyAS400/GrantCountyAs400.Web/Controllers/Assessment/LegalDocumentController.cs
@@ -0,0 +1,49 @@
+﻿using GrantCountyAs400.Domain.Assessment.Repository;
+using GrantCountyAs400.Web.ViewModels.Assessment.LegalDocument;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrantCountyAs400.Web.Controllers.Assessment
+{
+    [Route("legal-document")]
+    public class LegalDocumentController : Controller
+    {
+        private readonly ILegalDocumentRepository _legalDocumentRepository;
+
+        public LegalDocumentController(ILegalDocumentRepository legalDocumentRepository)
+        {
+            _legalDocumentRepository = legalDocumentRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index(LegalDocumentFilterViewModel filter)
+        {
+            RealPropertyLegalDocumentViewModel viewmodel = null;
+            if (filter.ParcelNumber > 0)
+            {
+                var legalDocument = _legalDocumentRepository.GetByParcelNumber(filter.ParcelNumber);
+                viewmodel = AutoMapper.Mapper.Map<RealPropertyLegalDocumentViewModel>(legalDocument);
+                ViewBag.DisplayResults = true;
+                ViewBag.EmptyResults = (viewmodel == null);
+            }
+            else
+            {
+                ViewBag.DisplayResults = false;
+            }
+
+            ViewBag.FilterViewModel = filter;
+            return View(viewmodel);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult Details(int id)
+        {
+            var entity = _legalDocumentRepository.Details(id);
+            if (entity == null)
+                return NotFound();
+
+            var viewmodel = AutoMapper.Mapper.Map<LegalDocumentDetailsViewModel>(entity);
+            return View(viewmodel);
+        }
+    }
+}
---
+++ GrantCountyAS400/GrantCountyAs400.Web/ViewModels/Assessment/LegalDocument/LegalDocumentFilterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrantCountyAs400.Web.ViewModels.Assessment.LegalDocument
+{
+    public class LegalDocumentFilterViewModel
+    {
+        [Display(Name = "Parcel")]
+        [Required(ErrorMessage = "Please enter value for Parcel Number")]
+        public decimal ParcelNumber { get; set; }
+    }
+}
