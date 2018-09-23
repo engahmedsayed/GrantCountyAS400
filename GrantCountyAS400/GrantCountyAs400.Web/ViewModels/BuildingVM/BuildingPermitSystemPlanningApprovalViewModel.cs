cb79f138fa9163e5a3f37cfc47426869742bc611
Ahmed Sayed Mostafa <ahmed.sayed@itworx.com>
2018-09-23 13:12:57 +0300

    Fix missing properties in building details

--- GrantCountyAS400/GrantCountyAs400.Web/ViewModels/BuildingVM/BuildingPermitSystemPlanningApprovalViewModel.cs
+++ GrantCountyAS400/GrantCountyAs400.Web/ViewModels/BuildingVM/BuildingPermitSystemPlanningApprovalViewModel.cs
@@ -17,22 +17,32 @@ namespace GrantCountyAs400.Web.ViewModels.BuildingVM
         [Display(Name ="Front Property Line Setback")]
         public decimal? FrontPropertySetback { get; set; }
 
+        [Display(Name ="Side or Flanking Street Setback")]
         public decimal? SideFlankingSetback { get; set; }
 
+        [Display(Name ="Side Property Line Setback")]
         public decimal? SidePropertySetback { get; set; }
 
+        [Display(Name ="Back Property Line Setback")]
         public decimal? BackPropertySetback { get; set; }
 
+        [Display(Name ="Flood Zone")]
         public string FloodZone { get; set; }
+        [Display(Name ="Flood Zone Map #")]
         public string FloodZoneMapNumber { get; set; }
+        [Display(Name ="Requires Land Use Permit? (Y/N)")]
         public string LUPermitRequired { get; set; }
 
+        [Display(Name ="Project Notes")]
         public string Comments { get; set; }
 
+        [Display(Name ="Approved By")]
         public string ApprovedBy { get; set; }
 
+        [Display(Name ="Chg User")]
         public string UserID { get; set; }
 
+        [Display(Name ="Chg Date")]
         public DateTime? ChangeDate { get; set; }
     }
 }
