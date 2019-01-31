a81d62a2772a682b2e73b776d3a2ab06a65e4acd
Ebram Khalil <ebram_tharwat@hotmail.com>
2019-01-31 23:39:19 +0200

    [UI] Update displayed columns of Excise Tax Rates

--- GrantCountyAS400/GrantCountyAs400.Web/ViewModels/TreasurerVM/ExciseTaxRate/ExciseTaxRateViewModel.cs
+++ GrantCountyAS400/GrantCountyAs400.Web/ViewModels/TreasurerVM/ExciseTaxRate/ExciseTaxRateViewModel.cs
@@ -9,14 +9,20 @@ namespace GrantCountyAs400.Web.ViewModels.TreasurerVM.ExciseTaxRate
         [Display(Name = "Record Type")]
         public string RecordTypeStateLocal { get; set; }
         public string AreaCode { get; set; }
+        [Display(Name = "Effective Code")]
+        [DisplayFormat(DataFormatString = "{0:F0}")]
         public decimal? EffectiveCode { get; set; }
         [Display(Name = "Description")]
         public string Description { get; set; }
         [Display(Name = "Rate")]
         public decimal? TaxRate { get; set; }
+        [Display(Name = "Penalty Rate 1")]
         public decimal? PenaltyRate { get; set; }
+        [Display(Name = "Penalty Rate 2")]
         public decimal? PenaltyRate2 { get; set; }
+        [Display(Name = "Penalty Rate 3")]
         public decimal? PenaltyRate3 { get; set; }
+        [Display(Name = "Interest Rate")]
         public decimal? InterestRate { get; set; }
         public string InterestMethod { get; set; }
         [Display(Name = "Change User")]
