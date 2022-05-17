76b17f995203bda1ef43dda61b35ecb62b3c7d50
Ebram Khalil <ebram_tharwat@hotmail.com>
2022-05-17 20:02:50 +0200

    Disable forgot password for non-admin users

--- GrantCountyAS400/GrantCountyAs400.Web/ViewModels/AccountVM/ChangePasswordViewModel.cs
+++ GrantCountyAS400/GrantCountyAs400.Web/ViewModels/AccountVM/ChangePasswordViewModel.cs
@@ -4,10 +4,11 @@ namespace GrantCountyAs400.Web.ViewModels.AccountVM
 {
     public class ChangePasswordViewModel
     {
-        [Display(Name = "Current Password")]
         [Required]
+        [Display(Name = "Current Password")]
         public string OldPassword { get; set; }
 
+        [Required]
         [Display(Name = "New Password")]
         public string NewPassword { get; set; }
     }
